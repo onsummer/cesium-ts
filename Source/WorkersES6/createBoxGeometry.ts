50f727cba3717e4e56624be0d8859914351a0822
Gypsophila <onsummer@foxmail.com>
2022-04-18 12:16:44 +0800

    update

--- Source/WorkersES6/createBoxGeometry.ts
+++ Source/WorkersES6/createBoxGeometry.ts
@@ -1,9 +1,10 @@
 import BoxGeometry from '../Core/BoxGeometry'
-import defined from '../Core/defined'
 
-export default (boxGeometry, offset) => {
-  if (defined(offset)) {
-    boxGeometry = BoxGeometry.unpack(boxGeometry, offset);
+export default (boxGeometry: number[] | BoxGeometry, offset?: number) => {
+  if (boxGeometry instanceof BoxGeometry) {
+    return BoxGeometry.createGeometry(boxGeometry)
+  } else {
+    const _geometry = BoxGeometry.unpack(boxGeometry, offset)
+    return BoxGeometry.createGeometry(_geometry)
   }
-  return BoxGeometry.createGeometry(boxGeometry);
 }
